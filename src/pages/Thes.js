4712554d422801ea62e877ed7ec735dca96ba600
Hazel Windle <hwindle83@googlemail.com>
2023-02-05 14:13:50 +0000

    Added the text search component

--- src/pages/Thes.js
+++ src/pages/Thes.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Heading from '../components/common/Heading';
+import TextSearch from '../components/common/TextSearch';
 
 const Thes = () => {
-  return (<Heading pageTitle={'Thesaurus'} />);
+  return (
+    <main>
+      <Heading pageTitle={'Thesaurus'} />
+      <TextSearch
+        styleName={'words-text-search'}
+        placeholder={'Type a word here'}
+        onChangeHandler={onSearchChange}
+        onSubmitHandler={handleSubmit}
+      />
+    </main>
+  );
 };
 
 export default Thes;
