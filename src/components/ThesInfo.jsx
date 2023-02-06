ba1159f1fd50341d707f7ddfcb13149f766563c6
Hazel Windle <hwindle83@googlemail.com>
2023-02-06 13:39:26 +0000

    Finally got rid of useState rerenders by using useEffect

--- src/components/ThesInfo.jsx
+++ src/components/ThesInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import ErrorMsg from './ErrorMsg';
 import SynonymList from './SynonymList';
 import AntonymList from './AntonymList';
@@ -10,38 +10,44 @@ const ThesInfo = (props) => {
   // atonyms etc
   const [antonyms, setAntonyms] = useState([]);
   // similar  words - synonyms
-  const [synWords, setSynWords] = useState([]); 
+  const [synWords, setSynWords] = useState([]);
   // error msgs
   const [errorMsg, setErrorMsg] = useState('');
 
   const url = `https://www.dictionaryapi.com/api/v3/references/ithesaurus/json/${searchWord}?key=${process.env.REACT_APP_MW_THES_API_KEY}`;
   const { data, loading, error } = useThesaurus(url);
-  //const {meta: thesData} = data; 
-  // get synonyms from data structure returned
-  try {
-    const { syns, ants } = data[0]?.meta;
-    if (syns !== undefined) {
-      setSynWords((prev) => (syns));
-    }
-    if (ants !== undefined) {
-      setAntonyms((prev) => (ants));
-    }
-  } catch {
-    console.log('Still loading data...');
-  }
 
-  if (error) {
-    setErrorMsg(() => {'Error on MW API fetch: '});
-    console.log('hello from error');
-  }
-  //console.log('hello outside of error, before return');
+  useEffect(() => {
+    // get synonyms from data structure returned
+    try {
+      const { syns, ants } = data[0]?.meta;
+      if (syns !== undefined) {
+        console.dir(syns.flat());
+        setSynWords(() => syns.flat());
+      }
+      if (ants !== undefined) {
+        console.dir(ants.flat());
+        setAntonyms(() => ants.flat());
+      }
+    } catch {
+      console.log('Still loading data...');
+    }
+
+    if (error) {
+      setErrorMsg(() => {
+        'Error on MW API fetch: ';
+      });
+      console.log('hello from error');
+    }
+    //console.log('hello outside of error, before return');
+  }, [data, error]);
 
   return (
     <section className='card'>
-      <h3 style={{textAlign: 'center', fontFamily: 'var(--body-font)'}}>
-        {searchWord.toTitleCase} 
+      <h3 style={{ textAlign: 'center', fontFamily: 'var(--body-font)' }}>
+        {searchWord.toTitleCase}
       </h3>
-      <div style={{minWidth: '90%', maxWidth: '900px', margin: '2rem auto'}}>
+      <div style={{ minWidth: '90%', maxWidth: '900px', margin: '2rem auto' }}>
         {loading ? <p>Words Loading</p> : ''}
         <SynonymList synWords={synWords} />
         <AntonymList antonyms={antonyms} />
