42a68dab842227cda9b27615d5aeb0996595766d
Hazel Windle <hwindle83@googlemail.com>
2023-02-07 19:03:34 +0000

    Rewrote useMemo on Scrabble  ShuffleLetters
    In component ShuffleLetters, rewrote letterShuffler
    function and added comments.

--- src/components/ShuffleLetters.jsx
+++ src/components/ShuffleLetters.jsx
@@ -10,26 +10,26 @@ const ShuffleLetters = (props) => {
   useEffect(() => {
     const strArr = searchWord.split('');
     setFirstLetterSet([...strArr]);
-    savedShuffles();
+    letterShuffler();
   }, [searchWord]);
 
-  const letterShuffler = () => {
+  const letterShuffler = useMemo(() => {
     const newArr = firstLetterSet.map((setOfLetters) => {
+      // sorts the array in a different way each time
+      // may only work for numbers. Otherwise, use custom
+      // sort function
       const newArr = setOfLetters.sorted(() => Math.random() - 0.5);
       return newArr;
     });
-    setShuffleArr((prev) => [...prev], newArr);
-  };
-
-  const savedShuffles = useMemo(() => {
-    letterShuffler();
-  }, [searchWord]);
+    console.dir(newArr);
+    //setShuffleArr((prev) => [...prev], newArr);
+  }, [firstLetterSet]);
 
   return (
     <div>
       <h3>Shuffled Letters</h3>
       <table>
-        {!shuffleArr ? <ErrorMsg errorText={'No scrabble letters entered'} /> : ''}
+        {!shuffleArr && <ErrorMsg errorText={'No scrabble letters entered'} />}
         {shuffleArr && shuffleArr
           .forEach((oneArr) => oneArr)
           .map((letter, i) => (
